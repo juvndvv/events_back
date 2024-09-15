0e85db39fb69ef5d221ac718ab7f083e39458b24
Juan Daniel Forner Garriga <jdanielforga@gmail.com>
2024-09-15 12:53:33 +0200

    feat: create backoffice product purchase command

--- app/Backoffice/BackofficeProductPurchases/Application/Create/CreateBackofficeProductPurchaseCommand.php
+++ app/Backoffice/BackofficeProductPurchases/Application/Create/CreateBackofficeProductPurchaseCommand.php
@@ -4,10 +4,23 @@ namespace App\Backoffice\BackofficeProductPurchases\Application\Create;
 
 class CreateBackofficeProductPurchaseCommand
 {
-    public function __construct(
+    private function __construct(
         public readonly string $productId,
         public readonly string $creatorId,
+        public readonly int $quantity,
+        public readonly string $name,
+        public readonly string $email,
     )
     {
+    }
+
+    public static function create(
+        string $productId,
+        string $creatorId,
+        int $quantity,
+        string $name,
+        string $email,
+    ): self {
+        return new self($productId, $creatorId, $quantity, $name, $email);
     }
 }
--- app/Backoffice/BackofficeProductPurchases/Application/Create/CreateBackofficeProductPurchaseCommandHandler.php
+++ app/Backoffice/BackofficeProductPurchases/Application/Create/CreateBackofficeProductPurchaseCommandHandler.php
@@ -2,7 +2,33 @@
 
 namespace App\Backoffice\BackofficeProductPurchases\Application\Create;
 
+use App\Backoffice\BackofficeProductPurchases\Domain\Service\BackofficeProductPurchaseCreator;
+use App\Backoffice\BackofficeProductPurchases\Domain\ValueObject\BackofficeProductPurchaseBuyerEmail;
+use App\Backoffice\BackofficeProductPurchases\Domain\ValueObject\BackofficeProductPurchaseBuyerName;
+use App\Backoffice\BackofficeProductPurchases\Domain\ValueObject\BackofficeProductPurchaseQuantity;
+use App\Shared\Domain\Identifier\ProductId;
+use App\Shared\Domain\Identifier\UserId;
+
 class CreateBackofficeProductPurchaseCommandHandler
 {
+    public function __construct(
+        private readonly BackofficeProductPurchaseCreator $creator
+    )
+    {
+    }
 
+    public function __invoke(CreateBackofficeProductPurchaseCommand $command): string
+    {
+        $purchase = $this->creator->__invoke(
+            productId: ProductId::create($command->productId),
+            creatorId: UserId::create($command->creatorId),
+            purchaseQuantity: BackofficeProductPurchaseQuantity::create($command->quantity),
+            name: BackofficeProductPurchaseBuyerName::create($command->name),
+            email: BackofficeProductPurchaseBuyerEmail::create($command->email),
+        );
+
+        // TODO publish events
+
+        return $purchase->getId();
+    }
 }
